a16b11d41bcf736df5306504a7ef07e91be75f28
Theo <theocaldas28@gmail.com>
2021-04-15 15:18:06 -0300

    Markup Page 1

--- theoPlayground/theoPlayground/PlaygroundBook/Chapters/Chapter1.playgroundchapter/Pages/PlaygroundPage1.playgroundpage/main.swift
+++ theoPlayground/theoPlayground/PlaygroundBook/Chapters/Chapter1.playgroundchapter/Pages/PlaygroundPage1.playgroundpage/main.swift
@@ -1,39 +1,29 @@
 /*:
-  - Note: For better experience use this Playground in split landscape
- # Objects
-For these experience try typing one of these:
  
- * callout(Objects):
+ #From the inside
+ Just like Bowe, you may often find yourself with repressed feelings in your chest. It might be hard to release the heaviness from inside. Then, the best you can do is to **SHOUT** it all out!
  
-    `.macbook`\
-    `.iPhone`\
-    `.camera`\
-    `.watch`
+ * Important:
+ Although it feels good to spill out these awful thoughts, it doesn't solve any problem. Your inner struggle will only vanish through communication. When possible, try talking about the things that bother you with friends, family or any person you trust and feel comfortable with.
  
  
-Change the subject and then click **Run My Code**!
-When you’re done, go to [next page](@next)! 😄
+Please, hit the "Run" button to help Bowe!\
+(Use Full Screen Mode for a better experience)\
+When you’re done, go to [next page](@next)!
 */
 //#-hidden-code
 import PlaygroundSupport
 import SpriteKit
 import BookCore
 //#-end-hidden-code
-//var myObject : Objects = /*#-editable-code*/.macbook/*#-end-editable-code*/
+
 
 //#-hidden-code
-// Load the SKScene from 'GameScene.sks'
 let sceneView = SKView(frame: CGRect(x:0 , y:0, width: 683, height: 1024))
 if let scene = Main1(fileNamed: "Main1") {
-    // Set the scale mode
     scene.scaleMode = .aspectFit
-//    scene.editableCode = myObject
-
-    // Present the scene
     sceneView.presentScene(scene)
 }
 
 PlaygroundSupport.PlaygroundPage.current.liveView = sceneView
-
-//The sound effect I used is Creative Commons that can be found at www.soundbible.com by the name “Blop" recorded by Mark DiAngelo.
 //#-end-hidden-code
